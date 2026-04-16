b290427a148971027d49b0986b28509a36725f1a
tetsup <58599705+tetsup@users.noreply.github.com>
2026-04-16 12:36:32 +0900

    refactor: type export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export type { Game, GameOptions, OnInitGame, OnTickGame, TransparentMode } from '@/types/engine';
-export { KeyAssignment } from '@/types/input';
+export type { KeyAssignment } from '@/types/input';
 export type { InputManager } from '@/input/input-manager';
 export { GameApp } from '@/engine/game-app';
 export { SoftPad } from '@/input/soft-pad';
