18cb247f644de3735cb4e9752225c29bc8202456
tetsup <58599705+tetsup@users.noreply.github.com>
2026-04-19 08:13:24 +0900

    feat: add export GameRenderer

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-export type { Game, GameOptions, OnInitGame, OnTickGame, TransparentMode } from '@/types/engine';
+export type { Game, GameOptions, GameRenderer, OnInitGame, OnTickGame, TransparentMode } from '@/types/engine';
 export type { KeyAssignment } from '@/types/input';
 export type { InputManager } from '@/input/input-manager';
-export { GameApp } from '@/engine/game-app';
 export { SoftPad } from '@/input/soft-pad';
+export { GameApp } from '@/engine/game-app';
 export { resolveTransparentMode } from '@/utils/transparent-mode';
